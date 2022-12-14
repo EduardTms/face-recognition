f9f3fac62bd6c50f5bdf981764b6b7615bc14db3
Eduard <timiseduard@yahoo.com>
2022-12-14 14:21:31 +0100

    addded semi functionality to linkForm and face detection api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 // Libraries
 import ParticlesBg from 'particles-bg';
@@ -8,17 +8,34 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/LinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+  }
+
+  render() {
+    return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       {/* <FaceRecognition /> */}
       <ParticlesBg type="cobweb" bg={true} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/LinkForm/ImageLinkForm.jsx
+++ src/components/LinkForm/ImageLinkForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className="f3 center">
@@ -9,10 +9,12 @@ const ImageLinkForm = () => {
             <div>
                 <input 
                 className="f4 pa2 w-70 center"
-                type="text" />
+                type="text"
+                onChange={ onInputChange } />
                 <br />
                 <button
                 className="w-30 grow f4 link ph3 pv2 dib black bg-light-pink mt2 ba b--near-white shadow-3 pointer"
+                onClick={ onButtonSubmit }
                 >Detect</button>
             </div>
         </div>
