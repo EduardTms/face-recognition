8f06c7f20bba2ab85d6d24b128a941d1501863d0
Eduard <timiseduard@yahoo.com>
2023-02-08 21:03:38 +0100

    added functionality to sign in and sign out buttons, no need for account input yet

--- src/App.js
+++ src/App.js
@@ -61,14 +61,14 @@ class App extends Component {
       .catch((err) => console.log(err));
   };
 
-  onRouteChange = () => {
-    this.setState({ route: "home" });
+  onRouteChange = (route) => {
+    this.setState({ route: route });
   };
 
   render() {
     return (
       <div className="App">
-        <Navigation />
+        <Navigation onRouteChange={this.onRouteChange} />
         {this.state.route === "signin" ? (
           <SignIn onRouteChange={this.onRouteChange} />
         ) : (
