d4fe929416408a00abd9b7f214e2429e2db2e143
Eduard <timiseduard@yahoo.com>
2023-02-08 20:50:35 +0100

    sign in form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 // Libraries
-import ParticlesBg from 'particles-bg';
-import Clarifai from 'clarifai';
+import ParticlesBg from "particles-bg";
+import Clarifai from "clarifai";
 // components
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/LinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import Api from './components/FaceDetectionAPI/Api';
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/LinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Api from "./components/FaceDetectionAPI/Api";
+import SignIn from "./components/SignIn/SignIn";
 
 const app = new Clarifai.App({
-  apiKey: '2ee21aaf86de4a07ba1402e145d6af85'
- });
+  apiKey: "2ee21aaf86de4a07ba1402e145d6af85",
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageURL: '',
-      box: {
-
-      },
-    }
+      input: "",
+      imageURL: "",
+      box: {},
+    };
   }
 
   calculateFaceLocation = (data) => {
-    const face = data.outputs[0].data.regions[0].region_info.bounding_box
-    const image = document.getElementById('inputimage');
+    const face = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
@@ -38,39 +37,44 @@ class App extends Component {
       topRow: face.top_row * height,
       // finding the parallel point of the leftCol, that is on the opposite side of the face
       // it's the same concept as before, just subtracting the width of the right column from the total width
-      rightCol: width - (face.right_col * width),
-      bottomRow: height - (face.bottom_row * height)
-    }
-  }
+      rightCol: width - face.right_col * width,
+      bottomRow: height - face.bottom_row * height,
+    };
+  };
 
   displayBoundingBox = (box) => {
-    this.setState({box: box});
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageURL: this.state.input})
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
-      .then(response => this.displayBoundingBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err))
-  }
+    this.setState({ imageURL: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayBoundingBox(this.calculateFaceLocation(response)),
+      )
+      .catch((err) => console.log(err));
+  };
 
   render() {
     return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <Api box={this.state.box} imageURL={this.state.imageURL}/>
-      <ParticlesBg type="cobweb" bg={true} />
-    </div>
-    )
+      <div className="App">
+        <Navigation />
+        <SignIn />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <Api box={this.state.box} imageURL={this.state.imageURL} />
+        <ParticlesBg type="cobweb" bg={true} />
+      </div>
+    );
   }
 }
 
