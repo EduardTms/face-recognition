f9dd389d6d6872e05e475df11a986254268bcdc3
Eduard <timiseduard@yahoo.com>
2023-02-22 00:39:56 +0100

    changed wording from rank to number of entries

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -7,7 +7,7 @@ import "./Rank.css";
 const Rank = ({ name, entries }) => {
   return (
     <div className="rank-container mt0 pt0 ba b--near-black br3 center shadow-2">
-      <div className="black f3">{`${name}, your current rank is...`}</div>
+      <div className="black f3">{`${name}, your number of entries is `}</div>
       <div className="black f1">{entries}</div>
     </div>
   );
