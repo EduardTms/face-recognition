a3966e51d1a821a5e621fe28b45335d8dc4e8da7
EduardTms <timiseduard@yahoo.com>
2023-02-24 21:52:53 +0100

    fixed error where name wouldn't display correctly in Rank component

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     this.setState({
       user: {
         id: data.id,
-        name: data.username,
+        name: data.name,
         email: data.email,
         entries: data.entries,
         joined: data.joined,
