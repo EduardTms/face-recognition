6e8e777fd0813819f90c514ef209179ac4ab9f71
EduardTms <timiseduard@yahoo.com>
2023-02-26 23:48:47 +0100

    fixed a bug where register could send empty users to db

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -36,7 +36,7 @@ class Register extends React.Component {
     })
       .then((response) => response.json())
       .then((user) => {
-        if (user) {
+        if (user.id) {
           this.props.loadUser(user);
           this.props.onRouteChange("home");
         }
@@ -75,7 +75,7 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" for="password">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
                   Password
                 </label>
                 <input
